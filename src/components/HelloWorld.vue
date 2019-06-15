fc93dd1516c5d4f6df26c746958ea3f920419880
zh <277708272@qq.com>
2019-06-15 12:03:23 +0800

    add get info by address

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,8 @@
         end-placeholder="结束日期"
       >
       </el-date-picker>
-      <el-button type="primary" @click="getPledgeNatOrNrNat('pledge', pledgeData.period)" round>get pledge nat</el-button>
+      <el-button type="primary" @click="getPledgeNatOrNrNat('pledge', pledgeData.period)" round>get pledge nat
+      </el-button>
       <div>
         <p v-show="pledgeData.show">
           {{pledgeData.str}}
@@ -48,7 +49,26 @@
         </p>
       </div>
     </div>
-
+    <div class="address-data" style="margin-top: 50px">
+      <el-date-picker
+        v-model="addressData.period"
+        type="datetimerange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        style="width: 350px"
+      >
+      </el-date-picker>
+      <el-input v-model="addressData.address" placeholder="enter address" style="width: 150px"></el-input>
+      <el-button type="primary" @click="getAddressData" round>get address data</el-button>
+      <div>
+        <p v-show="addressData.show">
+          {{addressData.nr.str}}
+          {{addressData.pledge.str}}
+          {{addressData.vote.str}}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -68,41 +88,46 @@
         testPeriod: [],
         pledgeData: {
           period: [],
-          pledgeList: [],
+          pledgeTxList: [],
           pledgeNat: 0,
           show: false,
-          topic: 'chain.contract.NATToken'
+          topic: 'chain.contract.NATToken',
+          str: ''
         },
         nrData: {
           period: [],
-          nrList: [],
+          nrTxList: [],
           nrNat: 0,
           show: false,
-          topic: 'chain.contract.NATToken'
+          topic: 'chain.contract.NATToken',
+          str: ''
         },
         voteData: {
           period: [],
-          voteList: [],
+          voteTxList: [],
           rewardNat: 0,
           burnNat: 0,
           show: false,
-          topic: 'chain.contract.vote'
+          topic: 'chain.contract.vote',
+          str: ''
         },
-        AddressData: {
+        addressData: {
           address: '',
           period: [],
           pledge: {
-            nat: 0
+            nat: 0,
+            str: ''
           },
           nr: {
-            nat: 0
+            nat: 0,
+            str: ''
           },
           vote: {
             rewardNat: 0,
             usedNat: 0,
-            burnNat: 0
+            burnNat: 0,
+            str: ''
           },
-          str: '',
           show: false
         }
       }
@@ -138,21 +163,25 @@
                       hash: list[j].hash,
                       timestamp: list[j].timestamp
                     }
-                    this.nrData.nrList.push(item)
+                    this.nrData.nrTxList.push(item)
                   }
                   if (json.function === 'triggerPledge' || json.Function === 'triggerPledge') {
                     let item = {
                       hash: list[j].hash,
                       timestamp: list[j].timestamp
                     }
-                    this.pledgeData.pledgeList.push(item)
+                    this.pledgeData.pledgeTxList.push(item)
                   }
                   if (json.Function === 'vote') {
+                    let argsList = JSON.parse(json.Args)
+                    console.log(argsList)
+                    let voteUsedNat = parseInt(argsList[argsList.length - 1] / 1000000000000000000)
                     let item = {
                       hash: list[j].hash,
-                      timestamp: list[j].timestamp
+                      timestamp: list[j].timestamp,
+                      usedNat: voteUsedNat
                     }
-                    this.voteData.voteList.push(item)
+                    this.voteData.voteTxList.push(item)
                   }
                 }
                 if (i === totalPage && j === list.length - 1) {
@@ -179,7 +208,7 @@
         let endTimestamp = period[1].getTime()
         let periodList = []
         if (tag === 'pledge') {
-          list = this.pledgeData.pledgeList
+          list = this.pledgeData.pledgeTxList
           topic = this.pledgeData.topic
           this.pledgeData.pledgeNat = 0
           for (let m = 0; m < list.length; m++) {
@@ -189,7 +218,7 @@
           }
         }
         if (tag === 'nr') {
-          list = this.nrData.nrList
+          list = this.nrData.nrTxList
           topic = this.nrData.topic
           this.nrData.nrNat = 0
           for (let n = 0; n < list.length; n++) {
@@ -213,15 +242,19 @@
                 let data = result.Produce.data
                 for (let k = 0; k < data.length; k++) {
                   if (tag === 'pledge') {
-                    this.pledgeData.pledgeNat += parseFloat(data[k].value)
+                    if (address.length === 0) {
+                      this.pledgeData.pledgeNat += parseFloat(data[k].value);
+                    }
                     if (address.length > 0 && address === data[k].addr) {
-                      this.AddressData.pledge.nat += parseFloat(data[k].value)
+                      this.addressData.pledge.nat += parseFloat(data[k].value)
                     }
                   }
                   if (tag === 'nr') {
-                    this.nrData.nrNat += parseFloat(data[k].value)
+                    if (address.length === 0) {
+                      this.nrData.nrNat += parseFloat(data[k].value);
+                    }
                     if (address.length > 0 && address === data[k].addr) {
-                      this.AddressData.nr.nat += parseFloat(data[k].value)
+                      this.addressData.nr.nat += parseFloat(data[k].value)
                     }
                   }
                 }
@@ -230,29 +263,36 @@
           });
         }
         this.sleep(3000).then(() => {
-          if (tag === 'nr') {
+          let startTime = this.dateFormat(startTimestamp)
+          let endTime = this.dateFormat(endTimestamp)
+          let startTimeMinus7 = this.dateFormat(startTimestamp - 3600 * 1000 * 24 * 7)
+          let endTimeMinus7 = this.dateFormat(endTimestamp - 3600 * 1000 * 24 * 7)
+          if (tag === 'nr' && address.length === 0) {
             this.nrData.str = '';
             this.nrData.show = false
-            this.nrData.str = 'NR计算周期：' + this.dateFormat(startTimestamp) + '-' + this.dateFormat(endTimestamp) + '\n\\'
+            this.nrData.str = 'NR计算周期: ' + startTime + '-' + endTime + '\n\\'
               + ', 通过NR发放的NAT数量: ' + parseInt(this.nrData.nrNat / 1000000000000000000)
             this.nrData.show = true
           }
-          if (tag === 'pledge') {
+          if (tag === 'pledge' && address.length === 0) {
             this.pledgeData.str = ''
             this.pledgeData.show = false
-            this.pledgeData.str = '质押周期：' + this.dateFormat(startTimestamp - 3600 * 1000 * 24 * 7) + '-' + this.dateFormat(endTimestamp - 3600 * 1000 * 24 * 7)
+            this.pledgeData.str = '质押周期: ' + startTimeMinus7 + '-' + endTimeMinus7
               + ', 通过该周期质押进行发放的NAT数量共有: ' + parseInt(this.pledgeData.pledgeNat / 1000000000000000000);
             this.pledgeData.show = true
           }
           if (address.length > 0) {
-            this.AddressData.str = ''
-            this.AddressData.show = false
-            this.AddressData.str = '地址: ' + address + ', 在'
+            this.addressData.pledge.str = ''
+            this.addressData.nr.str = ''
+            this.addressData.show = false
+            this.addressData.nr.str = '地址: ' + address + ', 在NR计算周期: ' + startTime + '-' + endTime + '\n\\'
+              + ', 通过NR发放的NAT数量: ' + parseInt(this.addressData.nr.nat / 1000000000000000000) + '\n\\'
+            this.addressData.pledge.str = '质押周期: ' + startTimeMinus7 + '-' + endTimeMinus7
+              + ', 通过该周期质押进行发放的NAT数量有: ' + parseInt(this.addressData.pledge.nat / 1000000000000000000)
           }
         })
       },
       getVoteNat(period, address = '') {
-        console.log('address: ', address.length)
         if (period.length !== 2) {
           this.$notify.error({
             title: 'failed',
@@ -262,7 +302,7 @@
           })
           return
         }
-        let list = this.voteData.voteList
+        let list = this.voteData.voteTxList
         let topic = this.voteData.topic
         this.voteData.rewardNat = 0
         this.voteData.burnNat = 0
@@ -274,6 +314,7 @@
             periodList.push(list[i])
           }
         }
+        console.log('periodList: ', periodList)
         for (let j = 0; j < periodList.length; j++) {
           axios.post(this.getEventByHashUrl,
             JSON.stringify({hash: periodList[j].hash}),
@@ -282,42 +323,66 @@
                 'Content-Type': 'application/json'
               }
             }).then(response => {
+            console.log('vote events: ', response)
             let events = response.data.result.events
             for (let k = 0; k < events.length; k++) {
               if (events[k].topic === topic) {
                 let result = JSON.parse(events[k].data)
-                this.voteData.rewardNat += parseFloat(result.reward)
                 let voteBurnNatList = result.data
-                this.voteData.burnNat += parseFloat(voteBurnNatList[1].nat)
+                if (address.length === 0) {
+                  this.voteData.rewardNat += parseFloat(result.reward);
+                  this.voteData.burnNat += parseFloat(voteBurnNatList[1].nat)
+                }
                 if (address.length > 0 && address === voteBurnNatList[1].addr) {
-                  this.AddressData.vote.burnNat += parseFloat(voteBurnNatList[1].nat)
-                  this.AddressData.vote.rewardNat += parseFloat(result.reward)
+                  this.addressData.vote.burnNat += parseFloat(voteBurnNatList[1].nat)
+                  this.addressData.vote.rewardNat += parseFloat(result.reward)
+                  this.addressData.vote.usedNat += periodList[j].usedNat
                 }
               }
             }
           })
         }
         this.sleep(3000).then(() => {
-          this.voteData.str = ''
-          this.voteData.show = false
-          this.voteData.str = '投票周期: ' + this.dateFormat(startTimestamp) + '-' + this.dateFormat(endTimestamp)
-            + ', 本周已投出的NAT中获得激励的数量: ' + parseInt(this.voteData.rewardNat / 10)
-          this.voteData.show = true
+          let startTime = this.dateFormat(startTimestamp)
+          let endTime = this.dateFormat(endTimestamp)
+          if (address.length === 0) {
+            this.voteData.str = '';
+            this.voteData.show = false
+            this.voteData.str = '投票周期: ' + startTime + '-' + endTime
+              + ', 本周已投出的NAT中获得激励的数量: ' + parseInt(this.voteData.rewardNat / 10)
+            this.voteData.show = true
+          }
+          if (address.length > 0) {
+            this.addressData.vote.str = ''
+            this.addressData.vote.str = '投票周期: ' + startTime + '-' + endTime
+              + ', 本周已投出的NAT中获得激励的数量: ' + parseInt(this.addressData.vote.rewardNat / 10)
+            this.addressData.show = true
+          }
         })
 
       },
       getAddressData() {
-        if (this.AddressData.period.length !== 2) {
+        if (this.addressData.period.length !== 2) {
           this.$message({
             type: 'error',
-            message: 'please select time first',
+            message: 'please select the time',
             duration: 1000
           })
           return
         }
-        let period = this.AddressData.period
-        let address = this.AddressData.address
+        if (this.addressData.address.length === 0 || this.addressData.address === undefined) {
+          this.$message({
+            type: 'error',
+            message: 'please enter the address',
+            duration: 1000
+          })
+          return
+        }
+        let period = this.addressData.period;
+        let address = this.addressData.address
         this.getPledgeNatOrNrNat('pledge', period, address)
+        this.getPledgeNatOrNrNat('nr', period, address)
+        this.getVoteNat(period, address)
       },
       dateFormat(timestamp) {
         console.log('timestamp: ', timestamp)
